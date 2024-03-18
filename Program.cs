7070deb1d18354f5fcd9ea0a77f141f4478178eb
Pakker <gfabinam@gmail.com>
2024-03-18 16:34:55 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                int sol = 0;
+                int number1 = Convert.ToInt32(args[0]);
+                string symbol = args[1];
+                int number2 = Convert.ToInt32(args[2]);
+                if (symbol == "+")
+                {
+                    sol = number1 + number2;
+                }
+                if (symbol == "-")
+                {
+                    sol = number1 - number2;
+                }
+                if (symbol == "/")
+                {
+                    sol = number1 / number2;
+                }
+                if (symbol == "*")
+                {
+                    sol = number1 * number2;
+                }
+                Console.WriteLine($"{args[0]} {symbol} {args[2]} = {sol}");
+
+            }
+
+        }
+
+    }
+}
